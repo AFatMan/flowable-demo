2677d954bffd9eec56a04cae376d705977f36946
Liujs <1453052796@qq.com>
2021-08-04 10:53:02 +0800

    rest支持

---
+++ src/main/java/com/example/flowabledemo/service/MyService.java
@@ -0,0 +1,63 @@
+package com.example.flowabledemo.service;
+
+import com.example.flowabledemo.entity.Person;
+import com.example.flowabledemo.repository.PersonRepository;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.task.api.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Administrator
+ */
+@Service
+// @Transactional
+public class MyService {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
+
+    // @Autowired
+    // private PersonRepository personRepository;
+
+    @Transactional
+    public void startProcess() {
+        runtimeService.startProcessInstanceByKey("oneTaskProcess");
+    }
+
+    @Transactional
+    public List<Task> getTasks(String assignee) {
+        return taskService.createTaskQuery().taskAssignee(assignee).list();
+    }
+
+    // public void startProcess(String assignee) {
+    //
+    //     Person person = personRepository.findByUsername(assignee);
+    //
+    //     Map<String, Object> variables = new HashMap<String, Object>();
+    //     variables.put("person", person);
+    //     runtimeService.startProcessInstanceByKey("oneTaskProcess", variables);
+    // }
+    //
+    // public List<Task> getTasks(String assignee) {
+    //     return taskService.createTaskQuery().taskAssignee(assignee).list();
+    // }
+    //
+    // public void createDemoUsers() {
+    //     if (personRepository.findAll().size() == 0) {
+    //         personRepository.save(new Person("jbarrez", "Joram", "Barrez", new Date()));
+    //         personRepository.save(new Person("trademakers", "Tijs", "Rademakers", new Date()));
+    //     }
+    // }
+
+}
